3dcc00214056e15ee5282e2f867c9caa2b7ca767
Emmanuel Bustos <ema2159@gmail.com>
2021-02-12 21:32:47 +0100

    Mod: Adjust floor position

--- src/Exercises/Exercise1/scene.js
+++ src/Exercises/Exercise1/scene.js
@@ -43,7 +43,7 @@ const material3 = new THREE.MeshToonMaterial({color: 0x1ea8fc});
 const floorGeometry = new THREE.PlaneGeometry(60, 8);
 const floor = new THREE.Mesh(floorGeometry, material1);
 scene.add(floor);
-floor.position.set(27, -3, -2);
+floor.position.set(27, -3.5, -2);
 floor.rotation.x -= Math.PI / 2;
 
 const primitives = [];
