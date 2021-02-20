a8628c79861284c03471583cfc948d0a96f0d5b1
Emmanuel Bustos <ema2159@gmail.com>
2021-02-20 19:26:45 +0100

    Add: Exercise 4 scene.js boilerplate

---
+++ src/Exercises/Exercise4/scene.js
@@ -0,0 +1,41 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+import {OrbitControls} from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+
+// Setup scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x21272e);
+
+// Camera configuration
+// Parameters: FOV, aspect ratio, minimum rendering distance, maximum rendering distance
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.x = 0;
+camera.position.z = 0;
+
+// Renderer setup
+const renderer = new THREE.WebGLRenderer();
+// Set renderer size (window size)
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Setup orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, -2);
+controls.listenToKeyEvents(window); // optional
+
+// Append renderer to index.html body
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 1, 1).normalize();
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
